addb0893bd8742c5290defea7039aaf70740470a
Xuanyu Dong <xuanyudo@buffalo.edu>
2018-09-27 12:49:11 -0400

    optimized freq data + rule generate template

--- Apriori/apriori.py
+++ Apriori/apriori.py
@@ -1,13 +1,46 @@
 import numpy as np
 from collections import deque
+from timeit import default_timer as timer
+
+
+class Head:
+    def __init__(self, elem=[]):
+        self.elem = elem
+
+    def __len__(self):
+        return len(self.elem)
+
+
+class Body:
+    def __init__(self, elem=[]):
+        self.elem = elem
+
+    def __len__(self):
+        return len(self.elem)
+
+
+class Rule:
+    def __init__(self, head=None, body=None):
+        self.head = head
+        self.body = body
+        self.elem = head.elem.copy()
+        self.elem.extends(body.elem)
+
+    def __len__(self):
+        return len(self.head) + len(self.head)
 
 
 class Node:
-    def __init__(self, cols, sample, rows):
+    def __init__(self, parent, cols, sample, rows):
         self.cols = cols
         self.last_idx = cols[-1] + 1
         self.avaiable_rows = rows
-        self.sample = sample
+        if parent is not None:
+            self.sample = parent.sample.copy()
+            self.sample.extend(sample)
+        else:
+            self.sample = sample
+            # print(self)
 
     def __repr__(self):
         return "idx: {} \n ele: {}\n".format(str(self.cols), str(self.sample))
@@ -20,57 +53,73 @@ class apriori:
     def __init__(self, filename):
         self.data = np.genfromtxt(filename, delimiter='\t', dtype=str)
         self.freqset = {}
-        self.gen = {}
-        self.num_gen = len(self.data[0]) - 1
+        self.num_patient = len(self.data)
+        self.num_gen = len(self.data[0])
+        print("number of patient: {}\n number of gen: {}".format(self.num_patient, self.num_gen))
         self.freqlen = np.zeros(self.num_gen, dtype=int)
 
-    def apriori(self, sup):
+    def apriori(self, sup, conf):
+        self.reset()
+        self.gen_freq(sup)
+        self.gen_rule(conf)
+
+    def gen_freq(self, sup):
         self.reset()
         self.init_first_level(sup)
         i = 0
+        j = 0  # index of i's length freq set
         while len(self.freqset[i]) > 0:
-            temp_node = self.freqset[i].popleft()
+
+            temp_node = self.freqset[i][j]
             for n in range(temp_node.last_idx, self.num_gen, 1):
-                cols = self.obtain_gen_seq([n])
-                up = []
-                down = []
+                cols = self.obtain_gen_seq([n])  # time: 6 mins
+
+                args = {}
                 for elem in temp_node.avaiable_rows:
-                    if cols[elem] == "Up":
-                        up.append(elem)
+                    col = str(cols[elem][0])
+
+                    if col not in args:
+
+                        args[col] = [elem]
+
                     else:
-                        down.append(elem)
+                        args[col].append(elem)
 
-                if len(up) / self.num_gen >= sup:
-                    new_cols = temp_node.cols.copy()
-                    new_cols.append(n)
-                    self.freqset[i + 1].append(Node(new_cols, cols[up[0]], up))
-                if len(down) / self.num_gen >= sup:
-                    new_cols = temp_node.cols.copy()
-                    new_cols.append(n)
-                    self.freqset[i + 1].append(Node(new_cols, cols[down[0]], down))
+                for arg in args:
 
-            if len(self.freqset[i]) == 0 and len(self.freqset[i + 1]) != 0:
+                    if len(args[arg]) / self.num_patient >= sup:
+                        new_cols = temp_node.cols.copy()
+                        new_cols.append(n)
+                        self.freqset[i + 1].append(Node(temp_node, new_cols, [arg], args[arg]))
+            j += 1
+            if j == len(self.freqset[i]):
                 i += 1
+                j = 0
                 self.freqlen[i] = len(self.freqset[i])
+                if len(self.freqset[i]) == 0 or i == len(self.freqset):
+                    break
 
+        self.output_ans(sup)
         print(self.freqlen)
+
+    def gen_rule(self, conf):
+        pass
 
     def init_first_level(self, sup):
         for i in range(self.num_gen):
             cols = self.obtain_gen_seq([i])
             self.freqset[i] = deque()
-            up = []
-            down = []
-            for idx in range(len(cols)):
-                if cols[idx] == "Up":
-                    up.append(idx)
+            args = {}
+            for elem in range(len(cols)):
+                col = str(cols[elem][0])
+                if col not in args:
+                    args[col] = [elem]
                 else:
-                    down.append(idx)
-            if len(up) / self.num_gen >= sup:
-                self.freqset[0].append(Node([i], cols[up[0]], up))
-            if len(down) / self.num_gen >= sup:
-                self.freqset[0].append(Node([i], cols[down[0]], down))
-
+                    args[col].append(elem)
+            for arg in args:
+                # print("arg: {} {}".format(arg,len(args[arg])))
+                if len(args[arg]) / self.num_patient >= sup:
+                    self.freqset[0].append(Node(None, [i], [arg], args[arg]))
         self.freqlen[0] = len(self.freqset[0])
 
     def obtain_gen_seq(self, seq_col_idx, seq_row_idx=None):
@@ -78,11 +127,13 @@ class apriori:
             return self.data[:, seq_col_idx]
 
         return self.data[seq_row_idx, seq_col_idx]
+
     def reset(self):
         self.freqset = {}
         self.gen = {}
-        self.num_gen = len(self.data[0]) - 1
+        self.num_gen = len(self.data[0])
         self.freqlen = np.zeros(self.num_gen, dtype=int)
+
     def output_ans(self, sup):
         with open("freqset{}.txt".format(sup), "w") as f:
             f.write("Support is set to be {}%\n".format(sup * 100))
@@ -93,5 +144,7 @@ class apriori:
 
 if __name__ == '__main__':
     ap = apriori("associationruletestdata.txt")
-    ap.apriori(0.3)
-    ap.output_ans(0.3)
+    start = timer()
+    ap.apriori(0.3, 0.7)
+    end = timer()
+    print("time elapse: {}".format(end - start))
--- PCA/Code/pca1.py
+++ PCA/Code/pca1.py
@@ -6,12 +6,11 @@ class PCA:
     def __init__(self):
         self.data_list = []
         self.colorMap = {}
-        self.color = ['bo','ro','go','co','mo','yo','ko']
+        self.color = ['bo', 'ro', 'go', 'co', 'mo', 'yo', 'ko']
         self.feature_list = []
         self.disease = []
 
-    def pca(self,filename):
-        self.data_list = []
+    def pca(self, filename):
         self.data_list = self.read_file(filename)
         self.data_list = np.array(self.data_list)
         self.parse_feature_disease()
@@ -21,8 +20,8 @@ class PCA:
         value, vector = self.eigen(matrix_cov)
         y1 = self.fn(vector, value, variant_vec, 0)
         y2 = self.fn(vector, value, variant_vec, 1)
-        self.draw_graph(y1,y2)
-        #print("value: {} vector: {}".format(value, vector))
+        self.draw_graph(y1, y2)
+        # print("value: {} vector: {}".format(value, vector))
 
     def read_file(self, filename):
         ret = []
@@ -40,7 +39,7 @@ class PCA:
         self.feature_list = []
         for i in range(len(self.data_list[0])):
             if i == len(self.data_list[0]) - 1:
-                self.disease.append(self.data_list[:,i:i + 1].flatten())
+                self.disease.append(self.data_list[:, i:i + 1].flatten())
             else:
                 self.feature_list.append([float(x) for x in self.data_list[:, i:i + 1].flatten()])
 
@@ -72,28 +71,29 @@ class PCA:
     def eigen(self, cov):
         return np.linalg.eig(cov)
 
-    def fn(self, eigen_vector, eigen_value:np.ndarray, x,order):
+    def fn(self, eigen_vector, eigen_value: np.ndarray, x, order):
         y = []
         idx = eigen_value.argsort()[::-1][order]
-
         for i in range(len(x[0])):
             value = 0
             for j in range(0, len(x)):
-                value+= x[j][i] * eigen_vector[idx][j]
+                value += x[j][i] * eigen_vector[idx][j]
             y.append(value)
         return y
-    def draw_graph(self,y1,y2):
+
+    def draw_graph(self, y1, y2):
         self.colorMap = {}
         for i in range(len(self.disease[0])):
             if self.disease[0][i] in self.colorMap.keys():
-                plt.plot(y1[i],y2[i],self.colorMap[self.disease[0][i]])
+                plt.plot(y1[i], y2[i], self.colorMap[self.disease[0][i]])
             else:
                 self.colorMap[self.disease[0][i]] = self.color[len(self.colorMap)]
-                plt.plot(y1[i], y2[i],self.colorMap[self.disease[0][i]])
+                plt.plot(y1[i], y2[i], self.colorMap[self.disease[0][i]])
         plt.show()
+
+
 if __name__ == '__main__':
     pca = PCA()
     pca.pca("pca_a.txt")
     pca.pca("pca_b.txt")
     pca.pca("pca_c.txt")
-
